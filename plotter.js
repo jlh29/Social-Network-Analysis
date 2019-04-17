5388f22220b538f1c0cfa119be726c596cd3c4a7
JSierraAKAMC <39574094+JSierraAKAMC@users.noreply.github.com>
2019-04-17 06:37:18 -0400

    Successfully reads files into an array of Tweet objects and places them on a map. Now must filter through these to get rid of duplicate markers

--- plotter.js
+++ plotter.js
@@ -6,22 +6,28 @@ function convertCSVToObjs(csvContents){
 		return [];
 	var extractedObjects = [];
 	var headerElements = lines[0].split(",");
+	console.log("This file has " + headerElements.length + " columns");
 	for(var i = 1; i < lines.length; i++){
-		var currObject = {};
+		if(lines[i].length == 0)
+			continue;
+		if(i%10 == 0){
+			console.log("Processing line " + i);
+		}
+		var currObj = {};
 		var currLine = splitOuter(lines[i], ',');
 		if(currLine.length != headerElements.length){
 			console.log("Inspect current line; splitOuter does not return correct value.");
+			console.log(lines[i]);
+			console.log(currLine);
 			return;			
 		}
-		for(var j = 0; j < headerElements.length; i++){
-			currObj[j] = currLine[j];
-		}
-		currObj.label = currObj[0];
-		currObj.tweetObject = JSON.parse(currObj[1]);
-		currObj.inferedPoint = JSON.parse(currObj[2]);
-		currObj.localTime = currObj[3];
+		currObj.label = currLine[0];
+		currObj.tweetObject = JSON.parse(currLine[1]);
+		currObj.inferedPoint = JSON.parse(currLine[2]);
+		currObj.localTime = currLine[3];
 		extractedObjects.push(currObj);
 	}
+
 	return extractedObjects;
 }
 
@@ -58,21 +64,36 @@ function filterLabel(csvObjects, desiredLabel){
 //Custom function to split the example CSV file without external libraries. To fix any errors where the internal commas would 
 //interfere (such as within the tweet_object and infered_point columns), I make sure that the commas are outside of any object
 function splitOuter(str, delimiter){
+	var fixedStr = stripExtraQuotes(str);
 	var numOpenings = 0;
 	var splitResult = [];
 	var startIndex = 0;
-	for(var i = 0; i < str.length; i++){
-		if(str.charAt(i) == delimiter && numOpenings == 0){
-			splitResult.push(str.substring(startIndex, i-1));
+	for(var i = 0; i < fixedStr.length; i++){
+		if(fixedStr.charAt(i) == delimiter && numOpenings == 0){
+			splitResult.push(fixedStr.substring(startIndex, i));
 			startIndex = i+1;
-		} else if (str.charAt(i) == '{') {
+		} else if (fixedStr.charAt(i) == '{') {
 			numOpenings++;
-		} else if (str.charAt(i) == '}') {
+		} else if (fixedStr.charAt(i) == '}') {
 			numOpenings--;
 		}
 	}
-	splitResult.push(str.substring(startIndex));
+	splitResult.push(fixedStr.substring(startIndex));
 	return splitResult;
+}
+
+function stripExtraQuotes(str){
+	var tempStr = "";
+	var startIndex = 0;
+	for(var i = 0; i < str.length; i++){
+		if(str.charAt(i) == '"'){
+			tempStr += str.substring(startIndex, i);
+			i++;
+			startIndex = i;
+		}
+	}
+	tempStr += str.substring(startIndex);
+	return tempStr;
 }
 
 
@@ -97,6 +118,20 @@ function handleDragOver(event){
 	event.dataTransfer.dropEffect = "copy";
 }
 
+function addLocationMarkers(tweetObjects, geoJSONLayer){
+	var numAtLocation = new Map();
+	for(var i = 0; i < tweetObjects.length; i++){
+		if(numAtLocation.has(tweetObjects[i].inferedPoint)){
+			numAtLocation.set(tweetObjects[i].inferedPoint, numAtLocation.get(tweetObjects[i].inferedPoint)+1);
+		} else {
+			numAtLocation.set(tweetObjects[i].inferedPoint, 1);
+		}
+	}
+	for(var key of numAtLocation.keys()){
+		geoJSONLayer.addData(key);
+	}
+}
+
 window.onload = function(){
 	//Map
 	var map = L.map("map").setView([35,-95],3);
@@ -112,11 +147,6 @@ window.onload = function(){
 	var dropZone = document.getElementById("dropZone");
 	dropZone.addEventListener("dragover", handleDragOver, false);
 	dropZone.addEventListener("drop", handleFileSelect, false);
-
-
-
-
-
 
 
 	//AnalyzeButton
@@ -138,8 +168,6 @@ window.onload = function(){
 		}
 		//var tweets = convertCSVToObjs(csvContents);
 		//Get midpoint of all tweets
-		midpoint = [40.7128, -74.0060];
-		updateMap(map, midpoint, 7);
 		var tweetLayer = L.geoJSON().addTo(map);
 		var reader = new FileReader();
 		var csvText = "";
@@ -147,126 +175,13 @@ window.onload = function(){
 			csvText = reader.result;
 
 			var tweets = convertCSVToObjs(csvText);
-			console.log(tweets.length);
+			console.log("Gathered " + tweets.length + " tweets");
+			addLocationMarkers(tweets, tweetLayer);
+
+			map.fitBounds(tweetLayer.getBounds());
+			
 		};
 		reader.readAsText(inFile);
-
-		////When we have the file input done, we can use this
-		//Also need to take care of duplicates and add popup labels
-		/*for(var i = 0; i < tweets.length; i++){
-			tweetLayer.addData(getGeoJSONLocation(tweets[i]));
-		}
-		*/
-		var tempTweetLocations = [
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.24545549999999, 40.6242735], "type": "Point"},
-		{"coordinates": [-73.977717, 40.705597499999996], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-74.0108855, 40.787364999999994], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-74.184848, 40.734494], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.1581305, 40.859832], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-74.184848, 40.734494], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-74.1581305, 40.859832], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.16780750000001, 40.915047], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.19652450000001, 40.662479], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.82938399999999, 40.912499], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-74.184848, 40.734494], "type": "Point"},
-		{"coordinates": [-74.19652450000001, 40.662479], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.0245075, 40.7905115], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.9718795, 40.8895505], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.184848, 40.734494], "type": "Point"},
-		{"coordinates": [-74.19652450000001, 40.662479], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.214139, 40.766601], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-74.184848, 40.734494], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-74.19652450000001, 40.662479], "type": "Point"},
-		{"coordinates": [-73.9718795, 40.8895505], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.86085700000001, 40.944567500000005], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-73.94347789, 40.82757207], "type": "Point"},
-		{"coordinates": [-74.030317, 40.76662], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.046402, 40.885965999999996], "type": "Point"},
-		{"coordinates": [-73.9920625, 40.7145515], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.993189, 40.945262], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.16780750000001, 40.915047], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.16780750000001, 40.915047], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-74.066663, 40.7180015], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.1188015, 40.936252499999995], "type": "Point"},
-		{"coordinates": [-74.06378699999999, 40.778489], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"},
-		{"coordinates": [-73.8311875, 40.6708795], "type": "Point"},
-		{"coordinates": [-74.032258, 40.7463795], "type": "Point"},
-		{"coordinates": [-74.19652450000001, 40.662479], "type": "Point"},
-		{"coordinates": [-74.16780750000001, 40.915047], "type": "Point"},
-		{"coordinates": [-73.9685415, 40.780709], "type": "Point"},
-		{"coordinates": [-73.86085700000001, 40.944567500000005], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-73.9487755, 40.655138], "type": "Point"},
-		{"coordinates": [-74.16780750000001, 40.915047], "type": "Point"},
-		{"coordinates": [-73.8494415, 40.8503475], "type": "Point"}
-		];
-		for(var i = 0; i < tempTweetLocations.length; i++){
-			tweetLayer.addData(tempTweetLocations[i]);
-		}
-		map.fitBounds(tweetLayer.getBounds());
-
-
-
-		
 		
 	});
 
